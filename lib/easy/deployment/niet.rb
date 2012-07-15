d7aeaaf7cc3f9752e9d3b6bfa3f2f4008b19b0af
Shevaun Coker <shevaun.coker@abletech.co.nz>
2012-07-16 10:50:39 +1200

    make niet process count a variable so that apps and environment can have a configurable number of niet processes running. Fix restart job to actually restart the workers bump version

--- lib/easy/deployment/niet.rb
+++ lib/easy/deployment/niet.rb
@@ -2,25 +2,27 @@
 # To load this capistrano configuraiton, require 'easy/deployment/niet' from deploy.rb
 
 Capistrano::Configuration.instance(:must_exist).load do
+  set :niet_process_count, 2
+
   namespace :niet do
     desc "Setup Niet"
     task :setup, roles: :job do
       run "mkdir -p #{shared_path}/niet"
     end
 
-    desc "Starts the niet process monitor"
+    desc "Starts the niet process monitor and its jobs"
     task :start, roles: :job do
-      2.times do |i|
+      niet_process_count.times do |i|
         run "niet -p #{shared_path}/niet/jobs_worker_#{i}.pid -c #{current_path} bundle exec rake jobs:work RAILS_ENV=#{stage}"
       end
     end
 
-    desc "Restarts the niet process monitor"
+    desc "Restarts the processes running under niet"
     task :restart, roles: :job do
-      run "for job in #{shared_path}/niet/* ; do kill `cat $job`; done"
+      run "for job in #{shared_path}/niet/* ; do kill -TERM `cat $job`; done"
     end
 
-    desc "Stops the niet process monitor"
+    desc "Stops the processes running under niet and the niet process monitor"
     task :stop, roles: :job do
       run "for job in #{shared_path}/niet/* ; do kill -QUIT `cat $job`; done"
     end
