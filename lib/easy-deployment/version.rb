f153ffecb45d488d34f46c571eee909641d9fb46
Shevaun Coker <shevaun.coker@abletech.co.nz>
2013-01-11 12:02:24 +1300

    make niet roles a variable so that apps can specify additional roles if required

--- lib/easy-deployment/version.rb
+++ lib/easy-deployment/version.rb
@@ -1,5 +1,5 @@
 module Easy
   module Deployment
-    VERSION = "0.4.2"
+    VERSION = "0.4.3"
   end
 end
--- lib/easy/deployment/niet.rb
+++ lib/easy/deployment/niet.rb
@@ -3,6 +3,7 @@
 
 Capistrano::Configuration.instance(:must_exist).load do
   set :niet_process_count, 2
+  set :niet_roles, [:job]
 
   def remote_file_exists?(full_path)
     'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
@@ -10,12 +11,12 @@ Capistrano::Configuration.instance(:must_exist).load do
 
   namespace :niet do
     desc "Setup Niet"
-    task :setup, roles: :job do
+    task :setup, roles: niet_roles do
       run "mkdir -p #{shared_path}/niet"
     end
 
     desc "Starts the niet process monitor and its jobs"
-    task :start, roles: :job do
+    task :start, roles: niet_roles do
       if remote_file_exists?("#{shared_path}/niet")
         niet_process_count.times do |i|
           run "niet -p #{shared_path}/niet/jobs_worker_#{i}.pid -c #{current_path} bundle exec rake jobs:work RAILS_ENV=#{stage}"
@@ -26,7 +27,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     desc "Restarts the processes running under niet"
-    task :restart, roles: :job do
+    task :restart, roles: niet_roles do
       if remote_file_exists?("#{shared_path}/niet")
         run "for job in #{shared_path}/niet/* ; do kill -TERM `cat $job`; done"
       else
@@ -35,12 +36,12 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     desc "Stops the processes running under niet and the niet process monitor"
-    task :stop, roles: :job do
+    task :stop, roles: niet_roles do
       run "for job in #{shared_path}/niet/* ; do kill -QUIT `cat $job`; done"
     end
 
     desc "Diplays the status of the niet process monitor"
-    task :status, roles: :job do
+    task :status, roles: niet_roles do
       run "for job in #{shared_path}/niet/* ; do ps -fw `cat $job`; done"
     end
   end
