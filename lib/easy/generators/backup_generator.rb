23b77e634553b6c601db329aae720866de3814bd
Jeremy Olliver <jeremy.olliver@gmail.com>
2013-03-15 17:03:03 +1300

    Specifying compatibility for rails 3 & rails 4

--- lib/easy/generators/backup_generator.rb
+++ lib/easy/generators/backup_generator.rb
@@ -8,11 +8,36 @@ module Easy
 
     desc %{Generates a backup config set to run nightly and upload to S3}
 
+    RAILS_4_GEMS = {
+      "backup" => "~> 3.0.27",
+      "fog" => "~> 1.4.0",
+      "mail" => "~> 2.4.0"
+    }
+
+    RAILS_4_GEMS = {
+      "backup"  => "~> 3.1.3",
+      "fog"     => "~> 1.9.0",
+      "net-ssh" => "<= 2.5.2", # Greater than >= 2.3.0 as well, though we can't express that
+      "net-scp" => "<= 1.0.4", # Greater than 1.0.0
+      "excon"   => "~> 0.17.0",
+      "mail"    => "~> 2.5.0"
+    }
+
     def create_backup_files
+      gem_requirements = case Rails::VERSION::MAJOR
+      when 3
+        RAILS_4_GEMS
+      when 4
+        RAILS_4_GEMS
+      else
+        raise "Unsupported rails version - only rails 3 and rails 4 are supported"
+      end
+
       gem_group(:backup) do
-        gem "backup", "~> 3.1.3", :require => false
-        gem "fog",    "~> 1.9.0", :require => false
-        gem "whenever",           :require => false
+        gem "whenever", :require => false
+        gem_requirements.each do |gem_name, version_constraints|
+          gem gem_name, version_constraints, :require => false
+        end
       end
 
       template("backup.rb.tt", "config/backup.rb")
