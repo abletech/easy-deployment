569b8b7de913c34194bcf0c7b6b37862458e946d
Jeremy Olliver <jeremy.olliver@gmail.com>
2013-03-15 16:29:49 +1300

    Removing unused s3 symlinking deployment task

--- lib/easy/deployment/backup.rb
+++ lib/easy/deployment/backup.rb
@@ -6,14 +6,8 @@ Capistrano::Configuration.instance(:must_exist).load do
       task :setup, :except => { :no_release => true } do
         run "mkdir -p #{shared_path}/backup/data"
       end
-
-      desc "Adds a symbolic link from the s3.yml file in the shared/backup into the current/config folder"
-      task :symlink_s3_config do
-        run "ln -sf #{shared_path}/backup/s3.yml #{current_path}/config/s3.yml"
-      end
     end
   end
 
   after 'deploy:setup', 'easy:backup:setup'
-  after 'deploy:create_symlink', 'easy:backup:symlink_s3_config'
 end
